0d9158c58749a636dab18b476b016aad11481f29
jufer002 <jufer002@mail.goucher.edu>
2020-05-09 19:11:35 -0400

    Working app

--- web_app.py
+++ web_app.py
@@ -1,7 +1,16 @@
 from flask import *
+from elasticsearch_dsl import Search
+from elasticsearch_dsl.connections import connections
 
 
 app = Flask(__name__)
+
+# Connect to local host server
+connections.create_connection(hosts=['127.0.0.1'])
+
+
+g_predicate = ''
+g_args = []
 
 
 @app.route('/')
@@ -11,7 +20,62 @@ def search_page():
 
 @app.route('/results', methods=['GET', 'POST'])
 def results_page():
-    return render_template('page_results.html')
+    global g_predicate, g_args
+
+    # Get form info.
+    if request.method == 'POST':
+        predicate = request.form['predicate']
+        # Get all the arguments to the predicate.
+        # There are len(request.form)-1 arguments by the structure of the form.
+        args = [request.form[f'arg{arg_num + 1}'] for arg_num in range(len(request.form) - 1)]
+
+        g_predicate = predicate
+        g_args = args
+    else:
+        predicate = g_predicate
+        args = g_args
+
+    # Query over form info.
+    s = Search(index='covid_relation_index')
+
+    if predicate != '':
+        # Query over predicate.
+        s = s.query('query_string',
+                    query=predicate,
+                    fields=['predicate'],
+                    default_operator='or')
+
+    # Query over args.
+    for arg in args:
+        if arg == '':
+            continue
+        s = s.query('query_string',
+                    query=arg,
+                    fields=['arguments'],
+                    default_operator='or')
+
+    # Use query results to pass a result-list into the template.
+    response = s.execute()
+    result_list = {}
+    for hit in response.hits:
+        result = {
+            'score': hit.meta.score,
+            'doi': hit.doi,
+            'sent': hit.sent,
+            'predicate': hit.predicate,
+            'arguments': hit.arguments,
+        }
+
+        result_list[hit.meta.id] = result
+
+    num_results = response.hits.total['value']
+
+    # Set args to be two empty strings so that the values can be set in the form.
+    if len(args) == 0:
+        args = ['', '']
+
+    return render_template('page_results.html', num_results=num_results, result_list=result_list,
+                           predicate=predicate, args=args)
 
 
 @app.route("/docs/<result>", methods=['GET'])
