6738c16e662b24e8fb3c6e8095b1af365878bf1a
jufer002 <jufer002@mail.goucher.edu>
2020-05-09 15:06:12 -0400

    Page target template begun

--- web_app.py
+++ web_app.py
@@ -14,5 +14,9 @@ def results_page():
     return render_template('page_results.html')
 
 
+@app.route("/documents/<result>", methods=['GET'])
+def target_page():
+    return render_template('page_target.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
