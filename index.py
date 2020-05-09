76693db94687697c55b686319e94d6c898c02ea8
jufer002 <jufer002@mail.goucher.edu>
2020-05-09 17:06:35 -0400

    More work on the index

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import csv
+import time
 
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
@@ -23,9 +24,7 @@ argument_analyzer = analyzer('covid_argument_analyzer',
 
 class RelationDocument(Document):
     predicate = Text(analyzer=predicate_analyzer)
-
-    #                                 multi=True here v?
-    argument_analyzer = Text(analyzer=argument_analyzer)
+    arguments = Text(analyzer=argument_analyzer)
 
     def save(self, *args, **kwargs):
         return super(RelationDocument, self).save(*args, **kwargs)
@@ -46,9 +45,17 @@ def build_index():
     with open('data/relations.csv', newline='') as csvfile:
         reader = csv.reader(csvfile)
         header = next(reader)
+
         for i, cols in enumerate(reader):
+            metadata[str(i+1)] = {}
             for key, col in zip(header, cols):
-                metadata[str(i+1)][key] = col
+                if key == 'triple':
+                    # Get the predicate and the arguments.
+                    predicate, *args = col[2:-2].replace('\'', '').split(',')
+                    metadata[str(i+1)]['predicate'] = predicate
+                    metadata[str(i + 1)]['arguments'] = args
+                else:
+                    metadata[str(i+1)][key] = col
 
     def actions():
         for rel_id in range(1, len(metadata)+1):
@@ -61,7 +68,8 @@ def build_index():
                     'doi': metadata[dict_id]['doi'],
                     'doc_id': metadata[dict_id]['doc_id'],
                     'sent': metadata[dict_id]['sent'],
-                    'relation': metadata[dict_id]['triple']
+                    'predicate': metadata[dict_id]['predicate'],
+                    'arguments': metadata[dict_id]['arguments'],
                 }
             except ValueError:
                 continue
@@ -70,4 +78,6 @@ def build_index():
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     build_index()
+    print(f'=== Built index in {time.time() - start_time} seconds ===')
