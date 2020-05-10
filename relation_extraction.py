6691e94ce1500da8b9ca425f6d56f374bb7f80a8
sheetskristen <sheetskristen@gmail.com>
2020-05-10 18:47:54 -0400

    Augmented relationship extraction using dependency parses

--- relation_extraction.py
+++ relation_extraction.py
@@ -1,12 +1,24 @@
+import argparse
 import csv
-import re
 
 from tqdm import tqdm
 import jsonlines
 import spacy
 
-fi = 'data/CORD-NER-PROTEIN-500.jsonl'
-fo = 'data/relations.csv'
+# Command line arguments.
+parser = argparse.ArgumentParser(description='Create relationship .csv file using'
+                                             ' SciSpacy\'s en_ner_jnlpba_md NER model.')
+
+parser.add_argument('--in_file', type=str,
+                    default='data/CORD-NER-PROTEIN-corpus.jsonl',
+                    help='Filepath to jsonl file which contain the sentences to create relations from.')
+parser.add_argument('--out_file', type=str, default='data/relations.csv',
+                    help='csv output file to which CORD-NER protein entity-containing relations'
+                         'will be written (header: doc_id, sent, triple, analysis)')
+args = parser.parse_args()
+fi = args.in_file
+fo = args.out_file
+
 nlp = spacy.load('en_ner_jnlpba_md')
 
 def open_ner_data(fi):
@@ -22,25 +34,41 @@ def create_text_doc(data):
     # return ' '.join(text)
     return text
 
+def extract_text(data):
+    return [(doc['doc_id'], doc['sent'], doc['doi']) for doc in data]
+
 def parse(data):
     triples = []
-    text = create_text_doc(data)
-    for (sent, doc_id, sent_id) in tqdm(text, desc="Parsing sentences"):
+    text = extract_text(data)
+    for (doc_id, sent, doi) in tqdm(text, desc="Parsing sentences"):
         doc = nlp(sent)
         triple = {}
         for token in doc:
             if token.dep_ == "ROOT":
-                # token.lemma_
-                triple.update({'doc_id': doc_id, 'sent_id': sent_id})
+                triple.update({'doc_id': doc_id, 'sent': sent, 'doi': doi})
                 triple_lst = [token.lemma_]
+                relation_lst = [token]
                 for child in token.children:
-                    # if child.pos_ == "NOUN":
-                    left_edge = child.left_edge.i
-                    right_edge = child.right_edge.i + 1
-                    triple_lst.append(doc[left_edge:right_edge].text)
-                    # triple_lst.append(re.sub(r'\s\d*\s', '', doc[left_edge:right_edge].text))
-                triple.update({'triple': tuple(triple_lst)})
-        triples.append(triple)
+                    if child.pos_  not in ["PUNCT", "AUX"] and \
+                        child.text not in ['it', 'there'] and \
+                        child.dep_ != 'advmod': #exclude auxes, punc, expletives and adverbal modifiers
+
+                        left_edge = child.left_edge.i
+                        right_edge = child.right_edge.i + 1
+                        triple_lst.append(doc[left_edge:right_edge].text)
+                        relation_lst.append(doc[left_edge:right_edge])
+
+                if len(triple_lst) > 1:
+                    triple.update({'triple': triple_lst})
+                    token = relation_lst[0]
+                    dep_relations = [[token.text, token.dep_, token.head.text]]
+                    for span in relation_lst[1:]:
+                        span_lst =[]
+                        for token in span:
+                            span_lst.append((token.text, token.dep_, token.head.text))
+                        dep_relations.append(span_lst)
+                    triple.update({'analysis': dep_relations})
+                    triples.append(triple)
     return triples
 
 if __name__ == '__main__':
